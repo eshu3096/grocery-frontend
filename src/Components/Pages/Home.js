f6e4ff107feb40ab083b8e6054cb8aa0ff8d1348
eshu3096 <eshu0123@gmail.com>
2021-07-22 18:50:01 +0530

    initaial

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import images from '../img/bike.jpg';
 
 function Home() {
+
     return(
-        <div className="home">
+        <div className="home" style={{ backgroundImage: `url(${images})`}}>
             <h1>Home</h1>
+            <img src={images} />
         </div>
     );
 }
